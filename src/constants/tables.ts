2bbccbc2412eb0430fa6028f7508985355068499
Kaushik Sharma <kaushik8695@gmail.com>
2025-04-17 22:01:24 +0530

    Update tables.ts

--- src/constants/tables.ts
+++ src/constants/tables.ts
@@ -4,8 +4,8 @@ export default class Tables {
   static readonly posts = "posts";
   static readonly reactions = "reactions";
   static readonly comments = "comments";
-  static readonly reportedPosts = "reportedPosts";
-  static readonly reportedComments = "reportedComments";
-  static readonly reportedUsers = "reportedUsers";
-  static readonly userDeletionRequests = "userDeletionRequests";
+  static readonly reportedPosts = "reported_posts";
+  static readonly reportedComments = "reported_comments";
+  static readonly reportedUsers = "reported_users";
+  static readonly userDeletionRequests = "user_deletion_requests";
 }
