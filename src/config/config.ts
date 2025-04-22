6d4b00edb599eaa49ce29f12a430e9d641c66008
Kaushik Sharma <kaushik8695@gmail.com>
2025-04-22 23:58:18 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config({ path: `.env.${process.env.ENV!}` });
@@ -6,9 +7,29 @@ export default {
   development: {
     url: process.env.SUPABASE_CONNECTION_URI!,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(
+          process.env.SUPABASE_DB_SSL_CERT_FILE_NAME!,
+          "utf8"
+        ),
+      },
+    },
   },
   production: {
     url: process.env.SUPABASE_CONNECTION_URI!,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(
+          process.env.SUPABASE_DB_SSL_CERT_FILE_NAME!,
+          "utf8"
+        ),
+      },
+    },
   },
 };
